25b789607d031a8e7bbb20520a6a896dae75ea61
Marc Reichelt <mcreichelt@gmail.com>
2019-11-03 01:39:52 +0100

    Setup SharedCode module in iOS project

--- ios/GitHubAPI with Kotlin Multiplatform/ContentView.swift
+++ ios/GitHubAPI with Kotlin Multiplatform/ContentView.swift
@@ -7,10 +7,11 @@
 //
 
 import SwiftUI
+import SharedCode
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        Text(CommonKt.createApplicationScreenMessage())
     }
 }
 
