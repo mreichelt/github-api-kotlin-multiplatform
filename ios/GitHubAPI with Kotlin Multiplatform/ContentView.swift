632b966248555eb9a42a3858d7acab0951700b75
Marc Reichelt <mcreichelt@gmail.com>
2019-11-03 01:23:43 +0100

    #1 Add basic iOS project

---
+++ ios/GitHubAPI with Kotlin Multiplatform/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubAPI with Kotlin Multiplatform
+//
+//  Created by Marc Reichelt on 03.11.19.
+//  Copyright © 2019 Marc Reichelt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
